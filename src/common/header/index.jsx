5461a89286c8a6d2328c42b8ccd6419f45710ef1
zhang874010534 <z874010534@vip.qq.com>
2020-10-12 16:51:11 +0800

    'firstr'

---
+++ src/common/header/index.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import s from './style.module.scss';
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className={s.header}>
+        {/* 左边logo */}
+        <a href="/" className={s.logo}>
+          {' '}
+        </a>
+        {/* 注册登录 */}
+        <a href="/write" className={s.write} className={'btn'}>
+          写文章
+        </a>
+        <a href="/sign_up" className={s.sign_up}>
+          注册
+        </a>
+        <a href="/sign_in" className={s.log_in}>
+          登录
+        </a>
+        {/* 中间首页和瞎子啊 */}
+        <div className={s.nav}>
+          <div>
+            <span className={s.home}>首页</span>
+          </div>
+          <div className={s.download}>下载App</div>
+          <div>
+            <input
+              type="text"
+              name=""
+              id=""
+              className={s.search}
+              placeholder="搜索"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
