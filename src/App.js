a3f49089112d8859728001764024bcbc909f7115
zhang874010534 <z874010534@vip.qq.com>
2020-10-18 16:44:36 +0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Header from './common/header';
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/home';
+import Detail from './pages/detail';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +14,12 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Header></Header>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/detail" component={Detail}></Route>
+          </Switch>
+        </BrowserRouter>
       </Provider>
     );
   }
