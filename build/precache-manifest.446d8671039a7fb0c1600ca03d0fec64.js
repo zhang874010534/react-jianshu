1c73c7a1045408104c5e58d90b9225baffafb134
zhang874010534 <z874010534@vip.qq.com>
2020-10-19 21:35:29 +0800

    hashrouter

---
+++ build/precache-manifest.446d8671039a7fb0c1600ca03d0fec64.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aab101d7b2e726dfa68db896331310b",
+    "url": "/react-jianshu/build/index.html"
+  },
+  {
+    "revision": "7d5c0f1d79553e937e88",
+    "url": "/react-jianshu/build/static/css/main.26995515.chunk.css"
+  },
+  {
+    "revision": "7ac8098f5cdacf41cd87",
+    "url": "/react-jianshu/build/static/js/2.5e9a4fb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-jianshu/build/static/js/2.5e9a4fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5c0f1d79553e937e88",
+    "url": "/react-jianshu/build/static/js/main.45c52a75.chunk.js"
+  },
+  {
+    "revision": "502ed2c88305acca8a3b",
+    "url": "/react-jianshu/build/static/js/runtime-main.53e2e753.js"
+  },
+  {
+    "revision": "ef8cfe66107a098e4f872b57b3d4746e",
+    "url": "/react-jianshu/build/static/media/banner.ef8cfe66.png"
+  },
+  {
+    "revision": "6292e1b6fd9cbcc6542f5a8b6aa4de8b",
+    "url": "/react-jianshu/build/static/media/iconfont.6292e1b6.woff"
+  },
+  {
+    "revision": "8112c89eaa56e101751880c074cbb0e8",
+    "url": "/react-jianshu/build/static/media/iconfont.8112c89e.eot"
+  },
+  {
+    "revision": "a3a8da6e2faa7ea9e025ea7196486f76",
+    "url": "/react-jianshu/build/static/media/iconfont.a3a8da6e.svg"
+  },
+  {
+    "revision": "af1dd1ebae26cf914d59c428ee9e7514",
+    "url": "/react-jianshu/build/static/media/iconfont.af1dd1eb.ttf"
+  },
+  {
+    "revision": "576e85651e19159c3450fa1947cdac64",
+    "url": "/react-jianshu/build/static/media/list_img1.576e8565.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.90763c183732c6e9089f8a7d158efdfb.js"
+  "/react-jianshu/build/precache-manifest.446d8671039a7fb0c1600ca03d0fec64.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-jianshu/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
